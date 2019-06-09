434900ecefafbd20d80950534600a9f6213fc8b9
leonerd21707 <lr59252@gmail.com>
2019-06-08 23:58:48 -0300

    pastas padrão´ criadas

---
+++ BackEnd/pizzaria-extra-api/pizzaria-extra-api/Controllers/UsuariosController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace pizzaria_extra_api.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+    }
+}
